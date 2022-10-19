ab63d1d193b8aa088b1a5534c8f5b3902a085b38
fre-mont <lucysuh724@gmail.com>
2022-10-20 08:22:59 +0900

    hw2

--- hw2/02.py
+++ hw2/02.py
@@ -1,18 +1,14 @@
 # 신장과 체중데이터로 성별을 출력하는 퍼셉트론 => x1(학생의 신장) , x2(학생의 체중), y (성별_남/여)
-
 from matplotlib import test
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# 특징 데이터 : [x1, x2] 
-X = np.array([[160, 55], [163, 43], [165, 48],
-             [170, 80], [175, 76], [180, 70]])
+# 특징 데이터 : [x1, x2, x0] (x0은 바이어스 입력을 위함) 
+X = np.array([[160, 55, 1], [163, 43, 1], [165, 48, 1],   # -46  -35
+             [170, 80, 1], [175, 76, 1], [180, 70, 1]])   # 43.8
 
-# 라벨 데이터 : 여자는 0, 남자는 1
-y = np.array([0, 0, 0, 1, 1, 1])
-
-# 가중치 벡터 : 1로 초기화 
-W = np.ones(len(X[0])) 
+y = np.array([0, 0, 0, 1, 1, 1])  # 정답 데이터 : 여자는 0, 남자는 1
+W = np.array([1.0, 1.0, 0])  # 가중치는 [-1.0, 1.0], 바이어스 0으로 초기화 
 
 # 활성화 함수 : 계단함수 사용
 def step_func(t):
@@ -20,20 +16,26 @@ def step_func(t):
     else : return 0
     
 # 퍼셉트론 학습 알고리즘
-def perceptron_fit(X, y, epochs=10):
+def perceptron_fit(X, y, epochs=20):
     global W
     eta = 0.2 # 학습률 
-    # epochs 수만큼 학습 진행
     for i in range(1, epochs+1):
-        print("-"*60)
+        print("-"*80)
         print("epoch :", i)
-        
         for i in range(len(X)):
             predict = step_func(np.dot(X[i], W))
             error = y[i] - predict # 오차(예측값과 정답의 차이)
-            W += eta*error*X[i] # 가중치 업데이트
-            print(f'입력 : {X[i]}, 정답 : {y[i]}, 출력 : {predict}, 변경된 가중치 : {W}')
+            W += eta*error*X[i] # 가중치 업데이트   
+            print(f'정답 : {y[i]}, 출력 : {predict}, 변경된 가중치 : {W}')
             
+perceptron_fit(X, y)
 
-perceptron_fit(X, y, 20)
+
+print(W)
+
+
+
+
+
+
 
--- hw2/05.py
+++ hw2/05.py
@@ -1,28 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 학습률 
-eta = [0.01, 0.05, 0.1, 0.5, 1.0]
-max_iterations = 100
-point = []
+eta = [0.01, 0.05, 0.1, 0.5, 1.0]  # 학습률
+max_iterations = 100  # 반복횟수 
 
 # 오차함수와 1차 도함수
-cost_func = lambda x: x*x-6*x+4
-gradient = lambda x: 2*x - 6
+def cost_func(x): return x*x-6*x+4
+def gradient(x): return 2*x - 6
 
+point = []
+
+# 가중치 업데이트 
 for t in eta:
     x = 10
     for i in range(max_iterations):
         x = x - t*gradient(x)
-        point.append(x)
-    
-arr = np.array(point)
+        point.append(x)  # 변경된 가중치 저장
 
-# 그래프 시각화 
+arr = np.array(point)  
+
+# 그래프 시각화
 for i in range(1, len(eta) + 1):
     plt.plot(cost_func(np.arange(11)), 'b')
     plt.title(f'learning rate: {eta[i-1]}')
-    plt.scatter(arr[(i-1)*100:i*100], cost_func(arr[(i-1)*100:i*100]), color='r')
+    
+    plt.scatter(arr[(i-1)*max_iterations:i*max_iterations],
+                cost_func(arr[(i-1)*max_iterations:i*max_iterations]), color='r')
     plt.show()
 
 
